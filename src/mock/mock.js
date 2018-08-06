2074a855056cdf587e3e34d535dc98d6ea3d8621
Se7suna <setsuna@wo.cn>
2018-08-06 11:49:13 +0800

    new

--- src/mock/mock.js
+++ src/mock/mock.js
@@ -3,7 +3,7 @@ const Random = Mock.Random
 Mock.mock('/address', Random.province() + Random.county())
 Mock.mock('/foodtype', [
         {
-            'img': '/static/img/a867c870b22bc74c87c348b75528djpeg.jpeg',
+            'img': './static/img/a867c870b22bc74c87c348b75528djpeg.jpeg',
             'title': '美食'
         },
         {
@@ -11,35 +11,35 @@ Mock.mock('/foodtype', [
             'title': '商超便利'
         },
         {
-            'img': '/static/img/60ac33f023d9074e13cd78f9b5964jpeg.jpeg',
+            'img': './static/img/60ac33f023d9074e13cd78f9b5964jpeg.jpeg',
             'title': '午餐'
         },
         {
-            'img': '/static/img/dd7c960f08cdc756b1d3ad54978fdjpeg.jpeg',
+            'img': './static/img/dd7c960f08cdc756b1d3ad54978fdjpeg.jpeg',
             'title': '果蔬生鲜'
         },
         {
-            'img': '/static/img/af108e256ebc9f02db599592ae655jpeg.jpeg',
+            'img': './static/img/af108e256ebc9f02db599592ae655jpeg.jpeg',
             'title': '医药健康'
         },
         {
-            'img': '/static/img/b02bd836411c016935d258b300cfejpeg.jpeg',
+            'img': './static/img/b02bd836411c016935d258b300cfejpeg.jpeg',
             'title': '大牌5折'
         },
         {
-            'img': '/static/img/c888acb2c8ba9e0c813f36ec9e90ajpeg.jpeg',
+            'img': './static/img/c888acb2c8ba9e0c813f36ec9e90ajpeg.jpeg',
             'title': '浪漫鲜花'
         },
         {
-            'img': '/static/img/b7ba9547aa700bd20d0420e1794a8jpeg.jpeg',
+            'img': './static/img/b7ba9547aa700bd20d0420e1794a8jpeg.jpeg',
             'title': '麻辣烫'
         },
         {
-            'img': '/static/img/ec21096d528b7cfd23cdd894f01c6jpeg.jpeg',
+            'img': './static/img/ec21096d528b7cfd23cdd894f01c6jpeg.jpeg',
             'title': '地方菜系'
         },
         {
-            'img': '/static/img/235761e50d391445f021922b71789jpeg.jpeg',
+            'img': './static/img/235761e50d391445f021922b71789jpeg.jpeg',
             'title': '披萨意面'
         },
         {
