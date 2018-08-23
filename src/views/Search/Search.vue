40e3939f63901feb23068cb2ea6b39a4bad15b80
Se7suna <setsuna@wo.cn>
2018-08-24 02:38:43 +0800

    v1

--- src/views/Search/Search.vue
+++ src/views/Search/Search.vue
@@ -4,7 +4,7 @@
         <div class="search_top">
             <div class="search_top_input">
                 <i class="iconfont icon-icon-"></i>
-                <input type="text" placeholder="输入商家、商品名称">
+                <input type="text" placeholder="输入商家、商品名称" v-model="search">
             </div>
             <div class="left" @click="$router.back()">
                 <i class="iconfont icon-fanhui"></i>
@@ -13,7 +13,7 @@
                 <span>搜索</span>
             </div>
         </div>
-        <div class="search_main">
+        <div class="search_main" v-if="!search">
             <p class="search_main_top">
                 热门搜索
             </p>
@@ -26,13 +26,52 @@
                 </span>
             </div>
         </div>
+        <ul class="search_item" v-else>
+            <li v-for="item of show" :key="item.id" @click="turn (item.id)">
+                <div class="left">
+                    <img :src="item.logo">
+                </div>
+                <div class="right">
+                    <div>
+                        <span class="name">{{item.name}}</span>
+                        <span class="jian" v-if="item.info.ad.jian">减</span>
+                        <span class="niao" v-if="item.info.sale.num % 3 === 1">蜂鸟</span>
+                    </div>
+                    <span class="ping">评价{{item.info.sale.score}}</span>
+                </div>
+            </li>
+        </ul>
     </div>
 </template>
 <script>
     import HeaderTop from '../../components/HeaderTop/HeaderTop.vue'
     export default {
+        data () {
+            return {
+                search: ''
+            }
+        },
+        methods: {
+            turn (id) {
+                this.$router.push('/shop/' + id)
+            }
+        },
         components: {
             HeaderTop
+        },
+        computed: {
+            show () {
+                let arr = []
+                let shops = this.$store.state.shops
+                if (this.search) {
+                    for (let i of shops) {
+                        if (i.name.indexOf(this.search) >= 0) {
+                            arr.push(i)
+                        }
+                    }
+                }
+                return arr
+            }
         }
     }
 </script>
@@ -99,5 +138,51 @@
         color: #666;
         background-color: #f7f7f7;
         margin-right: 15px;
+    }
+    .search_item {
+        margin-top: -12px;
+        li {
+            display: flex;
+            align-items: center;
+        }
+        .left {
+            padding: 0 18px;
+            img {
+                width: 28px;
+                height: 28px;
+            }
+        }
+        .right {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            flex: 1;
+            padding: 22px 0;
+            margin-right: 18px;
+            border-bottom: 1px solid #e3e3e3;
+            div {
+                display: flex;
+                align-items: center;
+            }
+        }
+        .name {
+            font-size: 14px;
+            color: #333;
+            margin-right: 8px;
+        }
+        .jian {
+            font-size: 10px;
+            color: #fff;
+            padding: 1px 2px;
+            margin-right: 8px;
+            background-color: #fa7047
+        }
+        .niao {
+            font-size: 10px;
+            color: #fff;
+            padding: 1px 2px;
+            margin-right: 8px;
+            background-color: #2395ff
+        }
     }
 </style>
